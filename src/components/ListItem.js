e3d47f9ba430040160de8453d2c6a6c4c04bd76d
ngadiono <ondoelratz@gmail.com>
2018-07-25 11:37:27 +0700

    renderList

---
+++ src/components/ListItem.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+import React, { Component } from 'react';
+
+class ListItem extends Component {
+  render() {
+    
+  }
+
+}
+
+export default ListItem;
